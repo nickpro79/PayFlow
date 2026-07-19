c3068fa507c901e83772fac4ae97cc7585324f52
Nikhil Praveen <nikhilpraveen7777@gmail.com>
2026-07-19 18:01:25 +0530

    Added basic structure

---
+++ src/PayFlow.PaymentService/Data/PaymentDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PayFlow.Shared;
+
+namespace PayFlow.PaymentService.Data
+{
+    public class PaymentDbContext : DbContext
+    {
+        public PaymentDbContext(DbContextOptions<PaymentDbContext> options) : base(options) { }
+        public DbSet<Payment> Payments => Set<Payment>();
+
+    }
+}
---
+++ src/PayFlow.Shared/Payment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayFlow.Shared
+{
+    public class Payment
+    {
+        public Guid Id { get; set; }
+        public string IdempotencyKey { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public string Currency { get; set; } = "USD";
+        public string Status { get; set; } = "PENDING";// PENDING, SUCCESS, FAILED
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
